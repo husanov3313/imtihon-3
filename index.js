1dc79aa185609f13f8cecdee9120afa4e647eb99
Husniddin <husanovhusniddin492@gmail>
2022-12-12 00:28:50 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,7 @@
+let clear1 = document.querySelector(".img2");
+let clear2 = document.querySelector(".clear");
+clear1.style.display ("none")
+ clear2.addEventListener("click", () =>{
+    clear1.innerHTML =  "";
+ });
+ console.log(123);
